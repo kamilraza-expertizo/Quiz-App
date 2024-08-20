5b8468a7adb874e2427349ed156bcf6ac5302cb3
kamilraza-expertizo <Kamil.raza@expertizo.pk>
2024-08-20 17:22:18 +0500

    status added to result

--- components/ScoreComponent.tsx
+++ components/ScoreComponent.tsx
@@ -3,20 +3,20 @@
 import { useEffect, useState } from 'react';
 
 const ScoreComponent = () => {
-  const [totalScore, setTotalScore] = useState<string | null>(null);
+  const [totalScore, setTotalScore] = useState<number | null>(null);
   const [username, setUsername] = useState<string | null>(null);
 
   useEffect(() => {
-    const score = localStorage.getItem("totalScore")
+    const score = localStorage.getItem("totalScore") || 0
     const name = localStorage.getItem("username")
 
-    setTotalScore(score);
+    setTotalScore(+score);
     setUsername(name);
   }, []);
 
   return (
     <div className='border-2 border-slate-300 rounded-xl p-10 shadow-md'>
-      <div className="text-xl font-medium">
+      <div className="text-xl font-medium flex flex-col gap-2">
         <h1 className='text-3xl font-bold mb-3'>Result</h1>
         <p>
           <span className="text-[#777777]">Name:{" "}</span>
@@ -28,6 +28,17 @@ const ScoreComponent = () => {
             Total Score:{" "}
           </span>
           <span className="text-primary">{totalScore || "No score available"}</span>
+        </p>
+
+        <p>
+          <span className="text-[#777777]">
+            Status:{" "}
+          </span>
+          <span className="text-primary">{totalScore ? totalScore > 10 ? 
+          <span className='text-green-600'>Passed</span>
+          :
+          <span className='text-red-600'>Failed</span>
+          : "No score available"}</span>
         </p>
       </div>
     </div>
